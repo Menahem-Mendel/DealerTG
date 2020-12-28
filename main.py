d53febb9215a9a37fc4de26bbeb7e190b3480b4a
bolts <boltsh61n@gmail.com>
2020-12-28 23:12:54 +0200

    v0.5

--- main.py
+++ main.py
@@ -54,6 +54,8 @@ def main():
     dp.add_handler(pages.profile.Profile)
     dp.add_handler(MessageHandler(Filters.all, plane_messages_handler))
 
+    # dp.add_error_handler(error_handler)
+
     updater.start_polling()
     updater.idle()
 
